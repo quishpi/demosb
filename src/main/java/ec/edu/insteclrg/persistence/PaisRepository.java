e4a034f18a743c0f458ffda4b2e997279e5b7bf7
Luis Quishpi <luis.quishpi@gmail.com>
2023-05-22 21:12:40 -0500

    Init project

---
+++ src/main/java/ec/edu/insteclrg/persistence/PaisRepository.java
@@ -0,0 +1,9 @@
+package ec.edu.insteclrg.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.insteclrg.domain.Pais;
+
+public interface PaisRepository extends JpaRepository<Pais, Long> {
+
+}
